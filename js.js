b0e1f9a3b7a80830465fbfbee8aff0516bb4ee7d
AngelaMallet <angelamallet@gmail.com>
2018-10-10 10:20:55 +0200

    ex3

---
+++ js.js
@@ -0,0 +1,6 @@
+// j'ouvre le document ou j'appelle l'id de lastname. quand
+// je tape suir le clavier je sollicite une action
+document.getElementById('lastname').addEventListener('keyup', function(){
+  // la fonction est : je crée un message dans une boite de dialogue
+alert(lastname.value);
+})
